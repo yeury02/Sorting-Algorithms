b9ecbc1a716b464bca89110fb9ce637c1ec47ebb
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-02-01 18:20:33 -0500

    bubble sort function

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+
+def bubbleSort(nums):
+
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                tmp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = tmp
+    return nums
+
+nums = [2,3,1,5,8,7,4,3]
+print(bubbleSort(nums))
